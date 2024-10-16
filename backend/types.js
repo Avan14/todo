e2cebd3eba7f654a64a39ea60f561aa88e817987
Avan Sahu <145996250+Avan14@users.noreply.github.com>
2024-10-17 02:38:50 +0530

    Update types.js
    defination added

--- backend/types.js
+++ backend/types.js
@@ -1,5 +1,5 @@
 const { z } = require('zod');
-
+// zod lets you define schemas for non sql databases and makes sure the data entered is valid
 const mySchema_todo_creation = z.object({
   title: z.string().min(1, "Title is required"),
   description: z.string().min(1, "Description is required") 
